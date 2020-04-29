9f41ac28b92552a2558321fc88bce5fe20040128
Guilherme Lopes <guilherme@netsoft.eti.br>
2020-04-29 19:02:12 -0300

    ajustes pagina inicial

--- src/pages/HeaderSystem/index.tsx
+++ src/pages/HeaderSystem/index.tsx
@@ -1,14 +1,9 @@
 import React from 'react';
 import './styles.css';
-import { PageHeader } from 'antd';
 
 const HeaderSystem = () => {
     return (
-        <PageHeader
-            className="site-page-header"
-            title="Title"
-            subTitle="This is a subtitle"
-        />
+        <div>Nome do sistema</div>
     );
 }
 
