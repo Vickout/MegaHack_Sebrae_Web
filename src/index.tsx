5e9a944335792ee3b04277d0fb952a5a8b637267
Guilherme Lopes <guilherme@netsoft.eti.br>
2020-04-29 19:41:45 -0300

    ajustes layout inicial

--- src/index.tsx
+++ src/index.tsx
@@ -1,25 +1,70 @@
-import { Layout } from 'antd';
 import "antd/dist/antd.css";
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
-import HeaderSystem from './pages/HeaderSystem';
-import SiderSystem from './pages/SiderSystem';
 
-const { Header, Sider, Content } = Layout;
+import { Layout, Menu, Breadcrumb } from 'antd';
+import {
+  DesktopOutlined,
+  PieChartOutlined,
+  FileOutlined,
+  TeamOutlined,
+  UserOutlined,
+} from '@ant-design/icons';
+import App from "./App";
 
-ReactDOM.render(
-  <React.StrictMode>
-  <div>
-  <Layout>
-    <Header><HeaderSystem /></Header>
-      <Layout>
-        <Sider><SiderSystem /></Sider>
-        <Content><App /></Content>
+const { Header, Content, Footer, Sider } = Layout;
+const { SubMenu } = Menu;
+
+class SiderPrincipal extends React.Component {
+  state = {
+    collapsed: false,
+  };
+
+  onCollapse = (collapsed: any) => {
+    console.log(collapsed);
+    this.setState({ collapsed });
+  };
+
+  render() {
+    return (
+      <Layout style={{ minHeight: '100vh' }}>
+        <Sider collapsible collapsed={this.state.collapsed} onCollapse={this.onCollapse}>
+          <div className="logo" />
+          <Menu theme="dark" defaultSelectedKeys={['1']} mode="inline">
+            <Menu.Item key="1" icon={<PieChartOutlined />}>
+              Option 1
+            </Menu.Item>
+            <Menu.Item key="2" icon={<DesktopOutlined />}>
+              Option 2
+            </Menu.Item>
+            <SubMenu key="sub1" icon={<UserOutlined />} title="User">
+              <Menu.Item key="3">Tom</Menu.Item>
+              <Menu.Item key="4">Bill</Menu.Item>
+              <Menu.Item key="5">Alex</Menu.Item>
+            </SubMenu>
+            <SubMenu key="sub2" icon={<TeamOutlined />} title="Team">
+              <Menu.Item key="6">Team 1</Menu.Item>
+              <Menu.Item key="8">Team 2</Menu.Item>
+            </SubMenu>
+            <Menu.Item key="9" icon={<FileOutlined />} />
+          </Menu>
+        </Sider>
+        <Layout className="site-layout">
+          <Header className="site-layout-background" style={{ padding: 0 }} />
+          <Content>
+            <Breadcrumb>
+              <Breadcrumb.Item>Página inicial</Breadcrumb.Item>
+              <Breadcrumb.Item>Pagina atual</Breadcrumb.Item>
+            </Breadcrumb>
+            <div className="site-layout-background" style={{ padding: 24, minHeight: 360 }}>
+              <App />
+            </div>
+          </Content>
+          <Footer>2020</Footer>
+        </Layout>
       </Layout>
-      </Layout>
-    </div>
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+    );
+  }
+}
 
+ReactDOM.render(<SiderPrincipal />, document.getElementById('root'));
